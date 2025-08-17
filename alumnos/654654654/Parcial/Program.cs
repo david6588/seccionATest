92944ea0ca8231a2fcb04a8e5863ed0bb9f848b6
david6588 <davidcarrera658@gmail.com>
2025-08-17 14:27:55 -0600

    dany gei

---
+++ alumnos/654654654/Parcial/Program.cs
@@ -0,0 +1,34 @@
+﻿using System;
+//inicio de programa
+class Program
+{
+    static void Main()
+    {
+        int[] numeros = new int[5];
+
+        Console.WriteLine("Ingrese 5 números enteros:");
+
+        for (int i = 0; i < 5; i++)
+        {
+            Console.Write($"Número {i + 1}: ");
+            while (!int.TryParse(Console.ReadLine(), out numeros[i]))
+            {
+                Console.Write("Entrada inválida. Por favor ingrese un número entero: ");
+            }
+        }
+
+        double promedio = CalcularPromedio(numeros);
+        Console.WriteLine($"El promedio es: {promedio:F2}");
+    }
+//calcular promedio
+    static double CalcularPromedio(int[] arreglo)
+    {
+        int suma = 0;
+        foreach (int num in arreglo)
+        {
+            suma += num;
+        }
+        return (double)suma / arreglo.Length;
+    }
+}
+//fin de programa
